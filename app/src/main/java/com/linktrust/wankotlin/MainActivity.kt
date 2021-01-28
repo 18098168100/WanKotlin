8377f18f4e07f8900f1b6f05fe6e429710e65689
haohai <luruiqian@gomeplus.com>
2021-01-28 17:27:36 +0800

    :测试"amend命令"  Please enter the commit message for your changes. Lines starting

--- app/src/main/java/com/linktrust/wankotlin/MainActivity.kt
+++ app/src/main/java/com/linktrust/wankotlin/MainActivity.kt
@@ -1,8 +1,10 @@
   package com.linktrust.wankotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
   class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
@@ -10,5 +12,28 @@ import android.util.Log
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.e(TAG,"这是我的第1次提交")
+        var result = replaceBlank("aaaa    bbbbb")
+
+        Log.e(TAG,result)
+
     }
+
+
+      private fun replaceBlank(str: String?): String? {
+          var result: String? = null
+          return if (str == null) {
+              result
+          } else {
+              val p: Pattern = Pattern.compile("\\s*|\t|\r|\n")
+              val m: Matcher = p.matcher(str)
+              result = m.replaceAll("")
+              result
+          }
+      }
+
+      private fun replaceBlank2(str: String): String {
+          val p: Pattern = Pattern.compile("\\s*|\t|\r|\n")
+          val m: Matcher = p.matcher(str)
+          return m.replaceAll("")
+      }
 }
