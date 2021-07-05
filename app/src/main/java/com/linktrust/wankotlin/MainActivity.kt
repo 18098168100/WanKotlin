121da466606450acd9790f58530ac0e68f07a5f3
haohai <luruiqian@gomeplus.com>
2021-07-05 14:39:45 +0800

    创建文件

--- app/src/main/java/com/linktrust/wankotlin/MainActivity.kt
+++ app/src/main/java/com/linktrust/wankotlin/MainActivity.kt
@@ -3,6 +3,7 @@
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.io.File
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
@@ -11,25 +12,30 @@ import java.util.regex.Pattern
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e(TAG,"这是我的第1次提交")
-        var result = replaceBlank("aaaa    bbbbb")
+        var filePath = filesDir.absolutePath
+        var file = File("$filePath/etc2")
+        if(!file.exists()){
+            Log.e("haohai","000000000")
+            file.mkdirs()
+        }else{
+            Log.e("haohai","11111")
+        }
 
-        Log.e(TAG,result)
+        var file2 = File("${file.absolutePath}/name2.txt")
+        if (file2.exists()){
+            Log.e("haohai","222222")
+        }else{
+            Log.e("haohai","333333")
+            file2.createNewFile()
+            Log.e("haohai","555555")
+        }
 
+        if (file2.exists()){
+            Log.e("haohai","666666")
+        }else{
+            Log.e("haohai","777777")
+        }
     }
-
-
-      private fun replaceBlank(str: String?): String? {
-          var result: String? = null
-          return if (str == null) {
-              result
-          } else {
-              val p: Pattern = Pattern.compile("\\s*|\t|\r|\n")
-              val m: Matcher = p.matcher(str)
-              result = m.replaceAll("")
-              result
-          }
-      }
 
       /**
        *
